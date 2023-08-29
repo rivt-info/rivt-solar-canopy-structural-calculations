dc11c349f8d2bf1d52871efcae277b63453c2d35
rholland <rholland@structurelabs.com>
2023-08-29 14:33:51 -0700

    update 2 files and delete 2 files

--- rv0101-Codes-and-Loads/rivt0101-Codes.py
+++ rv0101-Codes-and-Loads/rivt0101-Codes.py
@@ -13,11 +13,11 @@ rv.R("""Overview and Codes | notoc | 1
 # %%
 rv.I("""Governing Codes | default
 
-    || image | data/fig1.png | 75
+    || image | data0101/fig1.png | 75
     Wind load 1 _[f]
 
     
-    || image | data/fig2.png | 75 
+    || image | data0101/fig2.png | 75 
     Wind load 2 _[f]
 
     
