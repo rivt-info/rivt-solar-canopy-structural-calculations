3f44b3b3057fd553d5a35ad29dd8943fcc1360ee
rholland <rholland@structurelabs.com>
2025-02-09 03:44:16 -0800

    update settings.json, tasks.json and r0101-codes.py

--- d01-loads/r0101-codes.py
+++ d01-loads/r0101-codes.py
@@ -30,12 +30,13 @@ welded tube frame, and solar panel clips.
 
 | IMG | ins01/site01.png | Wind Load 2, .2
      
-     
+
 # | TEXT | ins01/example1.txt | 1:3 | plain
 
 # wt2 = a2 * dl2/2   _[S]
 
 """)
+
 
 # %%
 rv.I(""" --code tables | xoss | none 
@@ -60,6 +61,39 @@ Residential Codes and are summarized in the following tables.
 
 
 # %%
+rv.V("""Gravity Loads and Seismic Mass | pass | none
+
+     
+# | VCFG | default | default | 2,2
+  
+Test of values block _[E]
+     
+|| EVAL | default | 2     
+area1 = 10700*SF | SF | roof area      
+area2 = 10*FT * 10*FT | SF |  floor area       
+ht1 = 9.0*FT | FT | wall height         
+len1 = 110*FT | FT | interior wall length
+len2 = 155*FT | FT | exterior wall length
+udl1 = 12.2*PSF | PSF | description 
+
+
+Equation 1 _[E]     
+wt2 = area2 * floordl1 | KIPS | ACI-315-05 
+
+Equation 2 _[E]
+wt3 = area3 * floordl2 | KIPS | ACI-315-05 
+
+|| VALS | val01/aisc-16.0.csv:75-80 | 2
+     
+|| VALS | val01/dlextwall0.csv:2-4 | 2
+
+Exterior wall - total area load _[E]
+|| VALS | ins01/equation1.csv:2-6 | 2
+
+""")
+
+
+# %%
 rv.X(""" Abbreviations and References | oss | none
 
 **Reference Standards**
@@ -79,6 +113,8 @@ rv.X(""" Abbreviations and References | oss | none
 | TEXT | ins01/abbrev_math.tex:1-0 | latex
 
 """)
+
+
 # %%
 rv.X(""" Write
 
